10854cadd3420012775f1d830931aaec0da994c9
jugalshah14 <145976665+jugalshah14@users.noreply.github.com>
2023-09-25 18:41:39 +0530

    new

--- checkout.js
+++ checkout.js
@@ -12,26 +12,26 @@ document.addEventListener("DOMContentLoaded", () => {
     const table = document.createElement("table");
     table.className = "table";
     table.innerHTML = `
-      <thead>
-        <tr>
-          <th>Product Image</th>
-          <th>Product Name</th>
-          <th>Quantity</th>
-          <th>Price</th>
-        </tr>
-      </thead>
-      <tbody>
-     
-      </tbody>
-      <tfoot>
-        <tr>
-          <td></td>
-          <td></td>
-          <td><b>Total Price:</b></td>
-          <td id="total-price"><b> ₹</b></td>
-        </tr>
-      </tfoot>
-    `;
+              <thead>
+                <tr>
+                  <th>Product Image</th>
+                  <th>Product Name</th>
+                  <th>Quantity</th>
+                  <th>Price</th>
+                </tr>
+              </thead>
+              <tbody>
+             
+              </tbody>
+              <tfoot>
+                <tr>
+                  <td></td>
+                  <td></td>
+                  <td><b>Total Price:</b></td>
+                  <td id="total-price"><b> ₹</b></td>
+                </tr>
+              </tfoot>
+            `;
 
     const tbody = table.querySelector("tbody");
 
@@ -43,11 +43,13 @@ document.addEventListener("DOMContentLoaded", () => {
       const row = document.createElement("tr");
       const totalItemPrice = product.discountedPrice * product.quantity;
       row.innerHTML = `
-        <td><img src="${product.image}" alt="${product.title}" width="50" height="50"></td>
-        <td>${product.title}</td>
-        <td>${product.quantity}</td>
-        <td>${totalItemPrice} ₹</td>
-      `;
+                <td><img src="${product.image}" alt="${
+        product.title
+      }" width="50" height="50"></td>
+                <td>${product.title}</td>
+                <td>${product.quantity}</td>
+                <td>${totalItemPrice.toFixed(2)} ₹</td>
+              `;
       tbody.appendChild(row);
 
       // Add the total price for this item to the overall total price
@@ -60,5 +62,48 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // Append the table to the checkout-details div
     checkoutDetails.appendChild(table);
+
+    // Add an event listener to the "Place Order" button
+    const placeOrderButton = document.getElementById("place-order-button");
+    placeOrderButton.addEventListener("click", () => {
+      // Retrieve and display shipping information from the form
+      const shippingInfo = {
+        first_name: document.getElementById("first_name").value,
+        last_name: document.getElementById("last_name").value,
+        email: document.getElementById("email").value,
+        address: document.getElementById("address").value,
+        city: document.getElementById("city").value,
+        zip: document.getElementById("zip").value,
+      };
+
+      // Store shipping information in local storage
+      localStorage.setItem("shippingInfo", JSON.stringify(shippingInfo));
+
+      // Create and display a dialog box with the shipping information
+      const shippingInfoDialog = `
+                <div class="modal fade" id="shipping-info-dialog" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                  <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Shipping Information</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                          <span aria-hidden="true">&times;</span>
+                        </button>
+                      </div>
+                      <div class="modal-body">
+                        <p>Name : ${shippingInfo.first_name} ${shippingInfo.last_name}</p>
+                        <p>E-Mail : ${shippingInfo.email}</p>
+                        <p>Address : ${shippingInfo.address} , ${shippingInfo.city} , ${shippingInfo.zip}</p>
+                      </div>
+                      <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Conform</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              `;
+      document.body.insertAdjacentHTML("beforeend", shippingInfoDialog);
+      $("#shipping-info-dialog").modal("show");
+    });
   }
 });
