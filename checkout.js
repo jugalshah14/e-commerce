377c530595ca27798cb29d4765291074cebf7529
jugalshah14 <145976665+jugalshah14@users.noreply.github.com>
2023-09-26 11:12:20 +0530

    new

--- checkout.js
+++ checkout.js
@@ -1,14 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   const checkoutDetails = document.getElementById("checkout-details");
+  const placeOrderButton = document.getElementById("place-order-button");
+  const shippingInfoForm = document.getElementById("shipping-info-form");
 
-  // Retrieve cart data from local storage
   const cart = JSON.parse(localStorage.getItem("cart")) || [];
 
-  // Check if the cart is empty
   if (cart.length === 0) {
     checkoutDetails.innerHTML = "<p>Your cart is empty.</p>";
   } else {
-    // Create a table to display cart items
     const table = document.createElement("table");
     table.className = "table";
     table.innerHTML = `
@@ -35,13 +34,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const tbody = table.querySelector("tbody");
 
-    // Initialize total price to 0
     let totalPrice = 0;
 
-    // Populate the table with cart items and calculate the total price
     cart.forEach((product) => {
       const row = document.createElement("tr");
-      const totalItemPrice = product.discountedPrice * product.quantity;
+      const totalItemPrice =
+        parseFloat(product.discountedPrice) * parseInt(product.quantity);
       row.innerHTML = `
                 <td><img src="${product.image}" alt="${
         product.title
@@ -52,21 +50,17 @@ document.addEventListener("DOMContentLoaded", () => {
               `;
       tbody.appendChild(row);
 
-      // Add the total price for this item to the overall total price
       totalPrice += totalItemPrice;
     });
 
-    // Display the total price in the table footer
     const totalPriceCell = table.querySelector("#total-price");
     totalPriceCell.textContent = `${totalPrice.toFixed(2)} ₹`;
 
-    // Append the table to the checkout-details div
     checkoutDetails.appendChild(table);
 
-    // Add an event listener to the "Place Order" button
-    const placeOrderButton = document.getElementById("place-order-button");
-    placeOrderButton.addEventListener("click", () => {
-      // Retrieve and display shipping information from the form
+    placeOrderButton.addEventListener("click", (event) => {
+      event.preventDefault();
+
       const shippingInfo = {
         first_name: document.getElementById("first_name").value,
         last_name: document.getElementById("last_name").value,
@@ -76,34 +70,43 @@ document.addEventListener("DOMContentLoaded", () => {
         zip: document.getElementById("zip").value,
       };
 
-      // Store shipping information in local storage
-      localStorage.setItem("shippingInfo", JSON.stringify(shippingInfo));
+      // Check if any of the fields are empty
+      if (Object.values(shippingInfo).some((value) => value.trim() === "")) {
+        alert("Please fill in all fields.");
+      } else {
+        // Store shipping information in local storage
+        localStorage.setItem("shippingInfo", JSON.stringify(shippingInfo));
 
-      // Create and display a dialog box with the shipping information
-      const shippingInfoDialog = `
-                <div class="modal fade" id="shipping-info-dialog" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                  <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                      <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Shipping Information</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                          <span aria-hidden="true">&times;</span>
-                        </button>
-                      </div>
-                      <div class="modal-body">
-                        <p>Name : ${shippingInfo.first_name} ${shippingInfo.last_name}</p>
-                        <p>E-Mail : ${shippingInfo.email}</p>
-                        <p>Address : ${shippingInfo.address} , ${shippingInfo.city} , ${shippingInfo.zip}</p>
-                      </div>
-                      <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Conform</button>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              `;
-      document.body.insertAdjacentHTML("beforeend", shippingInfoDialog);
-      $("#shipping-info-dialog").modal("show");
+        // Create and display a dialog box with the shipping information
+        const shippingInfoDialog = `
+                            <div class="modal fade" id="shipping-info-dialog" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                        <!-- Modal content here -->
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLabel">Shipping Information</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <p>Name : ${shippingInfo.first_name} ${shippingInfo.last_name}</p>
+                                            <p>E-Mail : ${shippingInfo.email}</p>
+                                            <p>Address : ${shippingInfo.address}, ${shippingInfo.city}, ${shippingInfo.zip}</p>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-success" data-dismiss="modal">Confirm</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        `;
+
+        document.body.insertAdjacentHTML("beforeend", shippingInfoDialog);
+
+        // Show the modal
+        $("#shipping-info-dialog").modal("show");
+      }
     });
   }
 });
